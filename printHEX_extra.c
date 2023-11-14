0675f76d2c4a421cfb697859d5f6167d4eb6216a
Daniel <danielquist224@gmail.com>
2023-11-14 13:38:25 -0800

    printf

---
+++ printHEX_extra.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * printHEX_extra - This prints an hexgecimal number.
+ * @num: The number to print.
+ * Return: counter.
+ */
+int printHEX_extra(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
